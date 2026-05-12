cb92661ae2e20ae00bf88946e93e31260207f42b
jaymin1310 <jvsolanki2005@gmail.com>
2026-05-12 13:11:56 +0530

    first commit for trustly project

---
+++ trustly-backend/src/test/java/com/trustly/TrustlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
